7fb26be0e40344fc8c0f6e9d0de5e40d5437b07b
juan <juanbonami@gmail.com>
2021-05-18 10:17:06 -0500

    method returns number closest to 21 else returns 0

--- src/com/blackjack/uniquesum/blackjack.java
+++ src/com/blackjack/uniquesum/blackjack.java
@@ -2,10 +2,22 @@ package com.blackjack.uniquesum;
 
 public class blackjack {
 	
+	static int twentyOne(int num1, int num2) {
+		if (num1 <= 21 && num2 <= 21) {
+			if (num1 >= num2) {
+				return num1;
+			}else {
+				return num2;
+			}
+		}
+		else {
+			return 0;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(twentyOne(21,15));
 	}
 
 }
